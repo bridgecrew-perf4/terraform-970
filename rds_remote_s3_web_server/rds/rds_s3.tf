8c08887da7965a905bb924247dfc4ad5b43bc362
nazy67 <68819199+nazy67@users.noreply.github.com>
2021-02-17 13:45:02 -0600

    Delete rds_remote_s3_web_server directory

--- rds_remote_s3_web_server/rds/rds_s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "remote_rds_state" {
-  bucket = "${var.env}-remote_rds_bucket"
-}
-
-output "aws_remote_rds_bucket" {
-  description = "id of rds_s3 bucket"  
-  value = aws_s3_bucket.remote_rds_state.id
-}
--- rds_remote_s3_web_server/web_server/tfvars /ops.tf
+++
@@ -1,2 +0,0 @@
-env = "ops"
-instance_type = "t2.micro"
