98292d5b1fc405030d7f32d2431804928ce66fcc
nazy67 <68819199+nazy67@users.noreply.github.com>
2021-05-17 01:58:56 -0500

    Update remote_state.tf

--- modules/webserver/remote_state.tf
+++ modules/webserver/remote_state.tf
@@ -1,8 +1,16 @@
+# data "terraform_remote_state" "rds" {
+#   backend = "s3"
+#   config = {
+#     bucket = "terraform-nazy-state"
+#     key    = "${var.env}/rds.tfstate"
+#     region = "us-east-1"
+#   }
+# }
 data "terraform_remote_state" "rds" {
   backend = "s3"
   config = {
-    bucket = "terraform-nazy-state"
-    key    = "${var.env}/rds.tfstate"
-    region = "us-east-1"
+    bucket = var.remote_state["bucket"]
+    key    = var.remote_state["key"]
+    region = var.remote_state["region"]
   }
 }
