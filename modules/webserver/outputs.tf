c9bc16d4fca78de85e29a94031a09607b7916f57
nazy67 <68819199+nazy67@users.noreply.github.com>
2021-05-18 00:04:17 -0500

    Update outputs.tf

--- modules/webserver/outputs.tf
+++ modules/webserver/outputs.tf
@@ -1,19 +1,19 @@
-output "web_lt_id" {
+output "module_web_lt_id" {
   value = aws_launch_template.web_template.id
   description = "the name of webserver launch template id"
 }
 
-output "web_asg_arn" {
+output "module_web_asg_arn" {
   value = aws_autoscaling_group.web_asg.arn
   description = "webserver's asg id"
 }
 
-output "web_alb_arn" {
+output "module_web_alb_arn" {
   value = aws_lb.web_alb.arn
   description = "webserver's load balancer arn"
 }
 
-output "web_alb_dns" {
+output "module_web_alb_dns" {
   value = aws_lb.webserver_alb.dns_name
   description = "webserver's load balancer dns"
 }
