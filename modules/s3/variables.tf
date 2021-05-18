dbc0bbecdab5afca6891c785d7d201af7707b45d
nazy67 <nazykh67@gmail.com>
2021-05-17 20:31:36 -0500

    added s3_module_example

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -2,14 +2,17 @@ variable "s3_bucket_name" {
     description = "the name of s3 bucket"
     type = string
 }
+
 variable "versioning_enabled" {
     description = "enable versioning of s3 bucket"
     type = bool
     default = true
 }
+
 variable "principals" {
   type = list(string)
 }
+
 variable "env" {
   type = string
 }
