9b036ab4d9634b242fcb632f44ab23a1ea729e3e
nazy67 <68819199+nazy67@users.noreply.github.com>
2021-05-18 16:29:31 -0500

    Update outputs.tf
    modified outputs.tf

--- modules/webserver/outputs.tf
+++ modules/webserver/outputs.tf
@@ -17,3 +17,13 @@ output "module_web_alb_dns" {
   value = aws_lb.web_alb.dns_name
   description = "webserver's load balancer dns"
 }
+
+output "rds_address" { # rds endpoint
+    value = data.terraform_remote_state.rds.outputs.module_address
+    description = "this is the  address of rds instance"
+}
+
+output "rds_username" {
+    value = data.terraform_remote_state.rds.outputs.module_username
+    description = "this is the  address of rds instance"
+}
