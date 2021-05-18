d74f22889ba0e43b470128ca6b691a5ba45fc3b9
nazy67 <68819199+nazy67@users.noreply.github.com>
2021-05-18 00:34:56 -0500

    Update lt.tf

--- modules/webserver/lt.tf
+++ modules/webserver/lt.tf
@@ -1,9 +1,18 @@
 resource "aws_launch_template" "web_template" {
-  name_prefix   = "${var.env}-template-"  # when you close it with '-' the rest of the name will be created by aws and will be unique
+  name_prefix   = "${var.env}_web_template-"  # when you close it with '-' the rest of the name will be created by aws and will be unique
   image_id      = data.aws_ami.amazon_linux2.image_id
   instance_type = var.instance_type
-
+  vpc_security_group_ids = [aws_security_group.webserver_sg.id]
+  
   lifecycle {
     create_before_destroy = true
   }
+  user_data       = filebase64("${path.module}/user_data.sh")
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.env}_web_lt"
+    }
+  )
 }
